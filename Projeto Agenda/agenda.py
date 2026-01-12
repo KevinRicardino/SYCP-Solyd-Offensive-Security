dc191656aa6efd9a21c559cb1b9d4d469807acab
Kevin Ricardino <kevin.j.bernardes@gmail.com>
2026-01-12 03:14:52 -0300

    Modificação no menu e adicição de novas funções

--- Projeto Agenda/agenda.py
+++ Projeto Agenda/agenda.py
@@ -13,31 +13,53 @@ AGENDA['maria'] = {
 }
 
 def mostrar_contatos():
-    for contato in AGENDA:
-        buscar_contato(contato)
-        print('----------------------')
+    if AGENDA:
+        print()
+        for contato in AGENDA:
+            buscar_contato(contato)
+    else:
+        print('>>>> Agenda vazia.')
 
 
 def buscar_contato(contato):
-    print('Nome:', contato)
-    print('Telefone:', AGENDA[contato]['telefone'])
-    print('Email:', AGENDA[contato]['email'])
-    print('Endereco:', AGENDA[contato]['endereco'])
+    try:
+        print()
+        print('\t----------------------')
+        print('\tNome:', contato)
+        print('\tTelefone:', AGENDA[contato]['telefone'])
+        print('\tEmail:', AGENDA[contato]['email'])
+        print('\tEndereco:', AGENDA[contato]['endereco'])
+        print('\t----------------------')
+    except KeyError:
+        print('\t>>>> Contato inexistente.')
+    except Exception as error:
+        print('\t>>>> Um erro inesperado ocorreu.')
+        print(error)
 
 
+def incluir_editar_contato(contato):
+    telefone = input('>>>> Digite o nome do telefone: ')
+    email = input('>>>> Digite o nome do email: ')
+    endereco = input('>>>> Digite o nome do endereco: ')
 
-def incluir_editar_contato(contato, telefone, email, endereco):
     AGENDA[contato] = {
         'telefone': telefone,
         'email': email,
         'endereco': endereco,
     }
+    print()
     print(f">>>> Contato '{contato}' adicionado/editado com sucesso.")
 
 
 def excluir_contato(contato):
-    AGENDA.pop(contato)
-    print(f">>>> Contato{contato} excluido com sucesso.")
+    try:
+        AGENDA.pop(contato)
+        print(f">>>> Contato '{contato}' excluído com sucesso.")
+    except KeyError:
+        print('>>>> Contato inexistente.')
+    except Exception as error:
+        print('>>>> Um erro inesperado ocorreu.')
+        print(error)
 
 
 def imprimir_menu():
@@ -52,24 +74,45 @@ def imprimir_menu():
     print('='*50)
     print()
 
-imprimir_menu()
 
-opcao = input('>>> Escolha uma opção: ')
-if opcao == '1':
-    mostrar_contatos()
-elif opcao == '2':
-    contato = input('>>> Digite o nome do contato: ')
-    buscar_contato(contato)
-elif opcao == '3':
-    contato = input('>>> Digite o nome do contato: ')
-    telefone = input('>>> Digite o nome do telefone: ')
-    email = input('>>> Digite o nome do email: ')
-    endereco = input('>>> Digite o nome do endereco: ')
-    incluir_editar_contato(contato, telefone, email, endereco)
-elif opcao == '5':
-    contato = input('>>> Digite o nome do contato: ')
-    excluir_contato(contato)
-elif opcao == '0':
-    print('>>> Fechando programa...')
-else:
-    print('>>> Opção inválida. Tente novamente: ')
+while True:
+    imprimir_menu()
+
+    opcao = input('>>>> Escolha uma opção: ')
+    if opcao == '1':
+        mostrar_contatos()
+    elif opcao == '2':
+        contato = input('>>>> Digite o nome do contato: ')
+        buscar_contato(contato)
+    elif opcao == '3':
+        contato = input('>>>> Digite o nome do contato: ')
+
+        try:
+            AGENDA[contato]
+            print('>>>> Contato já existente.')
+        except KeyError:
+            incluir_editar_contato(contato)
+    elif opcao == '4':
+        contato = input('>>>> Digite o nome do contato: ')
+
+        try:
+            AGENDA[contato]
+            print('>>>> Editando contato:', contato)
+            incluir_editar_contato(contato)
+        except KeyError:
+            print('>>>> Contato inexistente.')
+    elif opcao == '5':
+        contato = input('>>>> Digite o nome do contato: ')
+        excluir_contato(contato)
+    elif opcao == '0':
+        print('>>>> Fechando programa...')
+        break
+    else:
+        print('>>>> Opção inválida. Tente novamente: ')
+
+    print()
+    print('=' * 50)
+    continuar = input('\n>>>> Deseja continuar usando a agenda? (s/n): ').lower()
+    if continuar != 's':
+        print('>>>> Encerrando agenda...')
+        break
